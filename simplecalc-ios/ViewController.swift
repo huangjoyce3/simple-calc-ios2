969f6bd2af322f995f70ad67ca9716791163fd19
huangjoyce3 <jhuang3@uw.edu>
2018-01-29 16:29:42 -0800

    basic operations

--- simplecalc-ios/ViewController.swift
+++ simplecalc-ios/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var firstNum:Int? = 0
+    var secondNum:Int? = 0
+    var operation:String? = nil
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func numberTap(_ sender: AnyObject) {
+        display.text = sender.currentTitle
+    }
+    @IBAction func operationTap(_ sender: UIButton) {
+        operation = sender.titleLabel?.text
+        firstNum = Int(display.text!)
+    }
+    @IBAction func equalsTap(_ sender: AnyObject) {
+        var total:Int! = 0
+        secondNum = Int(display.text!)
+        if operation == "+"{
+            total = firstNum! + secondNum!
+        } else if operation == "-" {
+            total = firstNum! - secondNum!
+        }else if operation == "/"{
+            total = firstNum! / secondNum!
+        }else if operation == "*"{
+            total = firstNum! * secondNum!
+        }else if operation == "%"{
+            total = firstNum! / secondNum!
+        }
+        display.text = "\(Int(total))"
+    }
 }
 
